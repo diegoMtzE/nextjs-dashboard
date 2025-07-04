9c56f9def46361e9429ef9e7cd4fffac160232c5
diegoMtzE <deme20cabezas@gmail.com>
2025-07-04 00:48:12 -0600

    se agregaron los estilos globales y se implementaron las fuentes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,13 @@
+import "./ui/global.css"
+import {nunito} from "./ui/font"
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${nunito.className} antialiased`}>
       <body>{children}</body>
     </html>
   );
---
+++ app/ui/font.tsx
@@ -0,0 +1,8 @@
+import { Ubuntu, Nunito} from 'next/font/google'
+
+export const nunito=Nunito({subsets:["latin"]})
+
+export const ubuntu=Ubuntu({
+    subsets: ["latin"],
+    weight: '400'
+})
